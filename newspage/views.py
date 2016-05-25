99f686d922e81fea3d646225c7d20f454a2ae714
Dantemun <dantemode@naver.com>
2016-05-25 16:06:07 +0900

    post_detail 페이지에 댓글 확인할 수 있도록 수정

--- newspage/views.py
+++ newspage/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 
-from .models import Post
+from .models import Post, Comment
 from .forms import PostForm, CommentForm
 
 # Create your views here.
@@ -14,8 +14,10 @@ def index(request):
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
+    comment_list = Comment.objects.all()
     return render(request, 'newspage/post_detail.html', {
         'post':post,
+        'comment_list' : comment_list,
         })
 
 def post_new(request):

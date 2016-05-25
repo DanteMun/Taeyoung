8814fba46736b5734090add183d5b31f6a0b2246
Dantemun <dantemode@naver.com>
2016-05-25 15:09:45 +0900

    Post 모델 생성 후 마이그레이션 작업

--- newspage/models.py
+++ newspage/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+from django.core.urlresolvers import reverse
 # Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    photo = models.ImageField()
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reserve( 'newspage:post_detail', args=[self.pk])

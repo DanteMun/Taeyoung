4e4867f1e85af2292469450bed72622c1199b4ce
Dantemun <dantemode@naver.com>
2016-05-25 15:20:29 +0900

    post_detail 페이지 생성 및 admin에 Post register 후 새로운 post 2개 등록

--- newspage/urls.py
+++ newspage/urls.py
@@ -4,5 +4,5 @@ from django.conf.urls import url
 urlpatterns = [
 
     url(r'^$','newspage.views.index', name="index"),
-
+    url(r'^(?P<pk>\d+)/$', 'newspage.views.post_detail', name="post_detail"),
 ]
--- newspage/views.py
+++ newspage/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
+from .models import Post
 # Create your views here.
 
 def index(request):
     return render(request, 'newspage/index.html')
 
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'newspage/post_detail.html', {
+        'post':post,
+        })
